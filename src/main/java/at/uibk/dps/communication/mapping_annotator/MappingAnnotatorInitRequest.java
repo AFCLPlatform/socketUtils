76f9164ca0998708741caee793a77a3e30bce67d
Fedor Smirnov <fedor@dps.uibk.ac.at>
2020-09-09 18:50:22 +0200

    extensions for the communication scheduler <-> mapping annotator

--- src/main/java/at/uibk/dps/communication/mapping_annotator/MappingAnnotatorInitRequest.java
+++ src/main/java/at/uibk/dps/communication/mapping_annotator/MappingAnnotatorInitRequest.java
@@ -1,6 +1,7 @@
 package at.uibk.dps.communication.mapping_annotator;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * The mapping {@link MappingAnnotatorInitRequest} is used to inform the
@@ -13,12 +14,15 @@ public class MappingAnnotatorInitRequest {
 	protected final MappingAnnotatorRequestType requestType;
 	protected final List<MappingAnnotatorRequestType> requestTypes;
 	protected final List<MappingStruct> mappings;
+	protected final Map<String, String> configurationAttributes;
 
 	protected MappingAnnotatorInitRequest(MappingAnnotatorRequestType requestType,
-			List<MappingAnnotatorRequestType> requestTypes, List<MappingStruct> mappings) {
+			List<MappingAnnotatorRequestType> requestTypes, List<MappingStruct> mappings,
+			Map<String, String> configurationAttributes) {
 		this.requestType = requestType;
 		this.requestTypes = requestTypes;
 		this.mappings = mappings;
+		this.configurationAttributes = configurationAttributes;
 	}
 
 	public List<MappingAnnotatorRequestType> getRequestTypes() {
@@ -27,5 +31,9 @@ public class MappingAnnotatorInitRequest {
 
 	public List<MappingStruct> getMappings() {
 		return mappings;
+	}
+
+	public Map<String, String> getConfigurationAttributes() {
+		return configurationAttributes;
 	}
 }
--- src/main/java/at/uibk/dps/communication/mapping_annotator/MappingAnnotatorRequestFactory.java
+++ src/main/java/at/uibk/dps/communication/mapping_annotator/MappingAnnotatorRequestFactory.java
@@ -1,9 +1,12 @@
 package at.uibk.dps.communication.mapping_annotator;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
+import at.uibk.dps.communication.entity.ConfigurationAttribute;
 import net.sf.opendse.model.Mapping;
 import net.sf.opendse.model.Resource;
 import net.sf.opendse.model.Task;
@@ -19,6 +22,25 @@ import net.sf.opendse.model.Task;
 public class MappingAnnotatorRequestFactory {
 
 	/**
+	 * 
+	 * Area for the declarations concerning the execution time requests.
+	 *
+	 */
+
+	public enum RegressorType {
+		DECISION_TREE, KNN, RANDOM_FOREST
+	}
+	
+	public enum CalculationTypeExecTime{
+		START_TIME, SUM
+	}
+
+	public static final String ATTR_NAME_ACTIVE_FUNCTION_PREDICTOR = "Active function predictor";
+	public static final String ATTR_NAME_RTT_PREDICTOR = "Rtt predictor";
+	public static final String ATTR_NAME_START_TIME_PREDICTOR = "Start time predictor";
+	public static final String ATTR_NAME_CALCULATION_TYPE_EXEC_TIME = "calculation type";
+
+	/**
 	 * Returns the request object to request the execution time of the given
 	 * mapping.
 	 * 
@@ -30,19 +52,31 @@ public class MappingAnnotatorRequestFactory {
 	}
 
 	/**
-	 * Returns the request object for the initiation of the (dynamic) mapping annotation by the annotator.
+	 * 
+	 * Area for the declarations concerning the init requests.
+	 *
+	 */
+
+	/**
+	 * Returns the request object for the initiation of the (dynamic) mapping
+	 * annotation by the annotator.
 	 * 
 	 * @param requestTypes the types of requests which are expected
-	 * @param mappings the mappings which will be requested
+	 * @param mappings     the mappings which will be requested
 	 * @return the request object for the init
 	 */
 	public static MappingAnnotatorInitRequest generateMappingInitRequest(Set<MappingAnnotatorRequestType> requestTypes,
-			Set<Mapping<Task, Resource>> mappings) {
+			Set<Mapping<Task, Resource>> mappings, Set<ConfigurationAttribute> configAttributes) {
 		List<MappingAnnotatorRequestType> typeList = new ArrayList<>(requestTypes);
 		List<MappingStruct> mappingList = new ArrayList<>();
 		for (Mapping<Task, Resource> m : mappings) {
 			mappingList.add(new MappingStruct(m));
 		}
-		return new MappingAnnotatorInitRequest(MappingAnnotatorRequestType.INIT_REQUEST, typeList, mappingList);
+		Map<String, String> configMap = new HashMap<>();
+		for (ConfigurationAttribute confAttr : configAttributes) {
+			configMap.put(confAttr.getKey(), confAttr.getValue());
+		}
+		return new MappingAnnotatorInitRequest(MappingAnnotatorRequestType.INIT_REQUEST, typeList, mappingList,
+				configMap);
 	}
 }
