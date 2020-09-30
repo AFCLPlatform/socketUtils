7229092feb510051b4328fee8a52631f0ea247ab
Fedor Smirnov <fedor@dps.uibk.ac.at>
2020-09-30 18:29:38 +0200

    slightly changed the scheduler resource selector communication

--- src/main/java/at/uibk/dps/communication/EnactmentEngineRequestFactory.java
+++ src/main/java/at/uibk/dps/communication/EnactmentEngineRequestFactory.java
@@ -6,41 +6,61 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 /**
- * This class is used to define the request which will be sent
- * to the enactment engine module.
+ * This class is used to define the request which will be sent to the enactment
+ * engine module.
  *
  * @author stefanpedratscher
  */
 public class EnactmentEngineRequestFactory {
 
-    /**
-     * Returns an enactment engine request by workflow file content.
-     */
-    public static EnactmentEngineRequest getEnactmentEngineRequestFileContent(byte[] fileContent) {
-        return new EnactmentEngineRequest(fileContent);
-    }
+	/**
+	 * Returns an enactment engine request by workflow file content.
+	 */
+	public static EnactmentEngineRequest getEnactmentEngineRequestFileContent(byte[] fileContent) {
+		return new EnactmentEngineRequest(fileContent);
+	}
 
-    /**
-     * Returns an enactment engine request by workflow file content and workflow input file.
-     */
-    public static EnactmentEngineRequest getEnactmentEngineRequestFileContent(byte[] fileContent, byte[] inputContent) {
-        return new EnactmentEngineRequest(fileContent, inputContent);
-    }
-    
-    /**
-     * Returns an {@link EnactmentEngineRequest} created from the workflow saved in the file with the provided path.
-     * 
-     * @param filePath the provided path name
-     * @return the {@link EnactmentEngineRequest} containing the wf as specified in the provided file.
-     * @throws IOException 
-     */
-    public static EnactmentEngineRequest getEnactmentEngineRequestFilePath(String filePath) throws IOException {
-    	File file = new File(filePath);
-    	FileInputStream fis = new FileInputStream(file);
-    	byte[] data = new byte[(int) file.length()];
-    	BufferedInputStream bis = new BufferedInputStream(fis);
-    	bis.read(data, 0, data.length);
-    	bis.close();
-    	return getEnactmentEngineRequestFileContent(data);
-    }
+	/**
+	 * Returns an enactment engine request by workflow file content and workflow
+	 * input file.
+	 */
+	public static EnactmentEngineRequest getEnactmentEngineRequestFileContent(byte[] fileContent, byte[] inputContent) {
+		return new EnactmentEngineRequest(fileContent, inputContent);
+	}
+
+	/**
+	 * Returns an {@link EnactmentEngineRequest} created from the workflow saved in
+	 * the file with the provided path.
+	 * 
+	 * @param filePath     the provided path name
+	 * @param inputContent the content of the workflow input (can be null)
+	 * @return the {@link EnactmentEngineRequest} containing the wf as specified in
+	 *         the provided file.
+	 * @throws IOException
+	 */
+	public static EnactmentEngineRequest getEnactmentEngineRequestFilePath(String filePath, byte[] inputContent)
+			throws IOException {
+		File file = new File(filePath);
+		FileInputStream fis = new FileInputStream(file);
+		byte[] data = new byte[(int) file.length()];
+		BufferedInputStream bis = new BufferedInputStream(fis);
+		bis.read(data, 0, data.length);
+		bis.close();
+		if (inputContent == null) {
+			return getEnactmentEngineRequestFileContent(data);
+		} else {
+			return getEnactmentEngineRequestFileContent(data, inputContent);
+		}
+	}
+
+	/**
+	 * See method above. Used in cases without the wokflow content.
+	 * 
+	 * @param filePath
+	 * @return
+	 * @throws IOException
+	 */
+	public static EnactmentEngineRequest getEnactmentEngineRequestFilePath(String filePath) throws IOException {
+		return getEnactmentEngineRequestFilePath(filePath, null);
+	}
 }
--- src/main/java/at/uibk/dps/communication/SelectorRequestFactory.java
+++ src/main/java/at/uibk/dps/communication/SelectorRequestFactory.java
@@ -3,24 +3,23 @@ package at.uibk.dps.communication;
 import at.uibk.dps.communication.entity.ResourceRequestType;
 
 /**
- * This class is used to define the request which will be sent
- * to the implementation selector module.
+ * This class is used to define the request which will be sent to the
+ * implementation selector module.
  *
  * @author stefanpedratscher
  */
 public class SelectorRequestFactory {
 
-    /**
-     * Return a selector request for free function resources.
-     */
-    public static SelectorRequest getFreeResourcesByFunctionType(String functionType) {
-        return new SelectorRequest(functionType, ResourceRequestType.GET_FREE_RESOURCES);
-    }
-
-    /**
-     * Return a selector request for all function resources.
-     */
-    public static SelectorRequest getAllResourcesByFunctionType(String functionType) {
-        return new SelectorRequest(functionType, ResourceRequestType.GET_ALL_RESOURCES);
-    }
+	/**
+	 * Returns the request for the resources for the provided function, following
+	 * the conditions formulated in the request type.
+	 * 
+	 * @param functionType the function type
+	 * @param requestType  the request type specifying the requested resources
+	 * @return the request for the resources for the provided function, following
+	 *         the conditions formulated in the request type
+	 */
+	public static SelectorRequest getResourcesByFunctionType(String functionType, ResourceRequestType requestType) {
+		return new SelectorRequest(functionType, requestType);
+	}
 }
